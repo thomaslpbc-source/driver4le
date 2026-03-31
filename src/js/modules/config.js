2929091a0cb4f72ccc860e10ee07abf14802d737
LE PABIC Thomas <lepabic@et.intechinfo.fr>
2026-03-31 15:24:37 +0200

    ajout pilotes

--- src/js/modules/config.js
+++ src/js/modules/config.js
@@ -2,11 +2,11 @@
 export const GAME_CONFIG = {
   maxAttempts: 6,
   clueOrder: ['outline', 'helmet', 'quote', 'teams', 'stats', 'nationality'],
-  rotationIntervalMs: 120 * 1000,
+  rotationIntervalMs: 60 * 1000,
   storagePrefix: 'driver4le',
 };
 
 export const UI_TEXT = {
   invalidDriver: 'Sélectionne un pilote valide dans la liste.',
-  alreadyPlayed: 'Cette rotation est terminée. Reviens à la prochaine rotation.',
+  alreadyPlayed: 'Reviens à la prochaine rotation.',
 };
